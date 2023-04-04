11a36b7782a950e14a8ddcc59a2d80fe6408f51c
Louie S <louie@example.com>
2023-04-03 20:40:22 -0700

    Changes to HTML layout

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,4 @@
 const endpoint = "http://127.0.0.1:8080/endpoints"
-let i = 0;
 
 
 function set_embed_page(url) {
@@ -7,12 +6,8 @@ function set_embed_page(url) {
 }
 
 function populateSearchBar(docset_json) {
-	//console.log(docset_json);
-	++i;
-	console.log(i);
-
-	// Add to search bar
-	document.getElementById("searchbar").getElementsByTagName("tbody")[0].innerHTML += "<tr>" +
+	// Add to docset-table
+	document.getElementById("docset-table").getElementsByTagName("tbody")[0].innerHTML += "<tr>" +
 		"<td scope=\"row\">" +
 		"<button id=\"docset-" + docset_json["CFBundleIdentifier"] + "-parent\" class=\"docset-parent\" onclick=\"set_embed_page('" + docset_json["docset_root"] + "')\">" +
 		docset_json["CFBundleName"] + 
